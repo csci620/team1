87f6d0592d6982137da3c517f6eca944a3e6e6d2
Jayesh-sathe <58150531+Jayesh-sathe@users.noreply.github.com>
2021-05-23 02:21:50 -0700

    Update app.js

--- app-main/api/app.js
+++ app-main/api/app.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
 });
 
 var whitelist = [
-  'https://csci620-team1-ui.azurewebsites.net'
+  'https://csci620-team1-ui.azurewebsites.net/*'
   
 ]
 var corsOptions = {
