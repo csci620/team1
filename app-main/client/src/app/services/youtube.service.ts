7c57ddf0af6406134fe80767b5fa68e9ff8976ad
Viraj-Sonavane <70547990+Viraj-Sonavane@users.noreply.github.com>
2021-05-24 02:22:46 -0700

    Blog API updated

--- app-main/client/src/app/services/youtube.service.ts
+++ app-main/client/src/app/services/youtube.service.ts
@@ -9,7 +9,7 @@ export class YoutubeService {
 
   constructor(private http:HttpClient) {}
       getChannels(channelName,find):Observable<any>{
-      const API_KEY = "AIzaSyDWP61IR3FRbIhc1wPGKbJR_ZN4Zopze5c"
+      const API_KEY = "AIzaSyDmRe6vHT2rM5ojvtMNrwaLYeHYlvGy0Bo"
       const url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+channelName+"&type=video&key="+API_KEY+"&q="+find+"&maxResults=2"
       return this.http.get<any>(url)
     }
